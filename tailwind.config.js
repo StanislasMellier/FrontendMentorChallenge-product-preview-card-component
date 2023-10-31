6b4d5e324a437ae135a2035302e5644b20234a47
StanislasMellier <95698594+StanislasMellier@users.noreply.github.com>
2023-10-31 17:57:24 +0100

    Project Complete

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				'very-dark-blue': 'hsl(212, 21%, 14%)',
+				'dark-grayish-blue': 'hsl(228, 12%, 48%)',
+				'Dark-cyan': 'hsl(158, 36%, 37%)',
+				Cream: 'hsl(30, 38%, 92%)',
+			},
+			fontFamily: {
+				Fraunces: ['Fraunces', 'serif'],
+				Montserrat: ['Montserrat', 'sans-serif'],
+			},
+		},
+		screens: {
+			md: '375px',
+		},
+	},
+	plugins: [],
+};
